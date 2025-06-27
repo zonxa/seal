6f72ddf840377ec4c12c00d5c448ef26a3c04de9
benr-ml <benriva@mystenlabs.com>
2025-06-27 13:39:43 +0300

    add comment

--- examples/frontend/src/utils.ts
+++ examples/frontend/src/utils.ts
@@ -132,6 +132,7 @@ export const getObjectExplorerLink = (id: string): React.ReactElement => {
 export const getKeyServers = (network: 'testnet' | 'mainnet') => {
 	if (network === 'testnet') {
 		return [
+      // Replace one of these with the object id of your key server.
 			'0x73d05d62c18d9374e3ea529e8e0ed6161da1a141a94d3f76ae3fe4e99356db75',
 			'0xf5d14a81a982144ae441cd7d64b09027f116a468bd36e7eca494f750591623c8',
 		];
